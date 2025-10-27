30ae76d39eff1e7f149da7f51bc47b36f6cfc11f
Neeraj yadav <Neeraj.yadav@teknopoint.local>
2025-10-27 14:03:00 +0530

    fetch api call common function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,6 +12,8 @@ import {
   loadCSS,
 } from './aem.js';
 
+import './common.js'
+
 /**
  * Moves all the attributes from a given elmenet to another given element.
  * @param {Element} from the element to copy attributes from
