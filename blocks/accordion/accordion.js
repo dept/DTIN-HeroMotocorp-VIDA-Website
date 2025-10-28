754c191a400d3b2387c4a0c4796105d13e40f309
Hrishikesh Shenoy <Hrishikesh.Shenoy@teknopoint.local>
2025-10-28 12:17:15 +0530

    changed tab js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -25,4 +25,26 @@ export default function decorate(block) {
     wrapper.append(details);
     row.replaceWith(wrapper);
   });
+
+  // View more / View less functionality
+  const dcw = document.querySelectorAll('.faq .default-content-wrapper')[1];
+  const viewMore = dcw.querySelectorAll('p:nth-child(1)')[0];
+  const viewLess = dcw.querySelectorAll('p:nth-child(2)')[0];
+  const height = document.querySelectorAll('.faq .accordion')[0];
+
+  viewMore.addEventListener('click', () => {
+    viewMore.style.display = 'none';
+    viewLess.style.display = 'block';
+    height.style.height = 'auto';
+  });
+
+  viewLess.addEventListener('click', () => {
+    viewLess.style.display = 'none';
+    viewMore.style.display = 'block';
+    if (window.matchMedia('(min-width: 900px)').matches) {
+      height.style.height = '628px';
+    } else {
+      height.style.height = '388px';
+    }
+  });
 }
