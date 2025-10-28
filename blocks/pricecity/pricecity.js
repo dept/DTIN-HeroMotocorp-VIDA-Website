4cd17c82a76d81a4c2e0cb1ec75c72fc5fb3e353
Hrishikesh Shenoy <Hrishikesh.Shenoy@teknopoint.local>
2025-10-28 18:46:33 +0530

    fixed css issues

--- blocks/pricecity/pricecity.js
+++ blocks/pricecity/pricecity.js
@@ -85,7 +85,7 @@ export default function decorateTable(block) {
     }
   });
 
-  if (window.innerWidth <= 768) {
+  if (window.innerWidth <= 900) {
     block.querySelectorAll('th').forEach((th) => {
       if (th.textContent.trim() === '') {
         th.remove();
